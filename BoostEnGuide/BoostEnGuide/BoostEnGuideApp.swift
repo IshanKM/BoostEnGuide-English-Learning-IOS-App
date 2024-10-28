e4e570a74167337478de828f1297aa82e354f9ca
IshanKM <ishanmadushanka.sg@gmail.com>
2024-10-28 08:11:23 +0530

    Create SwiftUI project

---
+++ BoostEnGuide/BoostEnGuide/BoostEnGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoostEnGuideApp.swift
+//  BoostEnGuide
+//
+//  Created by IKM on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BoostEnGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
