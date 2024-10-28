c228a6877f9b28b963bcfa26b3fa936eead4968e
IshanKM <ishanmadushanka.sg@gmail.com>
2024-10-29 00:23:49 +0530

    add animation to splash screen

--- BoostEnGuide/BoostEnGuide/UserInterface/SplashScreen.swift
+++ BoostEnGuide/BoostEnGuide/UserInterface/SplashScreen.swift
@@ -1,42 +1,50 @@
-//
-//  SplashScreen.swift
-//  BoostEnGuide
-//
-//  Created by IKM on 10/28/24.
-//
-
 import SwiftUI
 
 struct SplashScreen: View {
+    @State private var rotationAngle: Double = 0
+    @State private var isActive = false // State to track navigation to LoginScreen
+
     var body: some View {
         ZStack {
             Color(red: 0, green: 0.8, blue: 0.4) // Bright green background
                 .edgesIgnoringSafeArea(.all)
             
-            VStack {
-                ZStack {
-                    Circle()
-                        .stroke(Color.white, lineWidth: 4)
-                        .frame(width: 120, height: 120)
-                    
-                    Circle()
-                        .trim(from: 0.0, to: 0.75)
-                        .stroke(Color.white, lineWidth: 4)
-                        .frame(width: 120, height: 120)
-                        .rotationEffect(Angle(degrees: 135))
-                    
-                    VStack(spacing: 0) {
-                        Text("Boost on")
-                            .font(.system(size: 18, weight: .bold))
-                        Text("Guide")
-                            .font(.system(size: 18, weight: .bold))
+            if isActive {
+                loginScreen()
+                    .transition(.opacity) // Fade transition for LoginScreen
+            } else {
+                VStack {
+                    ZStack {
+                        Image("logoOne")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+                            .rotationEffect(.degrees(rotationAngle)) // Apply rotation effect
+                            .onAppear {
+                                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                                    rotationAngle = 360 // Rotate full circle continuously
+                                }
+                            }
+                        
+                        Image("logoTwo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
                     }
-                    .foregroundColor(.black)
+                }
+                .onAppear {
+                    // Delay of 3 seconds before navigating to LoginScreen
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            isActive = true
+                        }
+                    }
                 }
             }
         }
     }
 }
+
 
 struct SplashScreen_Previews: PreviewProvider {
     static var previews: some View {
